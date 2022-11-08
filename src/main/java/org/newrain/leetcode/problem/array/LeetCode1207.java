89278197382e77c0876f40342e793603a68bd501
newrain-zh <496268609@qq.com>
2022-11-08 22:43:25 +0800

    feat(all):新增算法题: leetcode-448等题

---
+++ src/main/java/org/newrain/leetcode/problem/array/LeetCode1207.java
@@ -0,0 +1,31 @@
+package org.newrain.leetcode.problem.array;
+
+import java.util.*;
+
+/**
+ * @author newRain
+ * @description 独一无二的出现次数
+ * @link <a href="https://leetcode.cn/problems/unique-number-of-occurrences/"></a>
+ */
+public class LeetCode1207 {
+
+    public boolean uniqueOccurrences(int[] arr) {
+        int length = arr.length;
+        if (length == 1) {
+            return true;
+        }
+        Map<Integer, Integer> map = new HashMap<>(length);
+        for (int v : arr) {
+            map.put(v, map.getOrDefault(v, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>(map.size());
+        for (Integer count : map.values()) {
+            if (set.contains(count)) {
+                return false;
+            } else {
+                set.add(count);
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/org/newrain/leetcode/problem/string/LeetCode1455.java
@@ -0,0 +1,20 @@
+package org.newrain.leetcode.problem.string;
+
+/**
+ * @author newRain
+ * @description 检查单词是否为句中其他单词的前缀
+ * @tag string
+ */
+public class LeetCode1455 {
+
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] s = sentence.split(" ");
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].startsWith(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
