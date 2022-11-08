15317d28cd0aa4f33b7d07641b57dff9b0d0011e
newRain <496268609@qq.com>
2022-11-08 22:43:25 +0800

    feat(all):新增算法题: leetcode-448等题

---
+++ src/main/java/org/newrain/leetcode/problem/array/LeetCode448.java
@@ -0,0 +1,31 @@
+package org.newrain.leetcode.problem.array;
+
+import java.util.*;
+
+/**
+ * @author newRain
+ * @description 找到所有数组中消失的数字
+ * @link <a href="https://leetcode.cn/problems/find-all-numbers-disappeared-in-an-array/"></a>
+ */
+public class LeetCode448 {
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int length = nums.length;
+        Set<Integer> e = new HashSet<>(length);
+        for (int num : nums) {
+            e.add(num);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= length; i++) {
+            if (!e.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+        System.out.println(findDisappearedNumbers(new int[]{1, 1}));
+    }
+}
