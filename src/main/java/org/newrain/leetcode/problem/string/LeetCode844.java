e96de35b022e2f562cc21ec81a23b16981843b2b
newrain-zh <496268609@qq.com>
2022-11-15 21:38:55 +0800

    feat(all):新增算法题: leetcode-844(简单-字符串)

---
+++ src/main/java/org/newrain/leetcode/problem/string/LeetCode844.java
@@ -0,0 +1,55 @@
+package org.newrain.leetcode.problem.string;
+
+/**
+ * @author newRain
+ * @description 比较含退格的字符串
+ * @link <a href="https://leetcode.cn/problems/backspace-string-compare/"><a/>
+ */
+public class LeetCode844 {
+
+
+    public static boolean backspaceCompare(String s, String t) {
+        String str1 = remove(s);
+        String str2 = remove(t);
+        System.out.println(str1);
+        System.out.println(str2);
+        return str1.equals(str2);
+    }
+
+    /**
+     * 该解题思路为借用StringBuilder删除#和前面的字符
+     * 最后比较字符串 也可以使用char数组来赋空字符串手动删除字符串比较
+     *
+     * @param str
+     * @return
+     */
+    public static String remove(String str) {
+        if (!str.contains("#")) {
+            return str;
+        }
+        StringBuilder stringBuilder = new StringBuilder(str);
+        while (stringBuilder.indexOf("#") >= 0) {
+            int index = stringBuilder.indexOf("#");
+            if (index == 0) {
+                stringBuilder.deleteCharAt(index);
+            } else {
+                stringBuilder.delete(index - 1, index + 1);
+            }
+        }
+        return stringBuilder.toString();
+
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(backspaceCompare("ab#c", "ad#c"));
+//        System.out.println(backspaceCompare("a#c", "b"));
+//        System.out.println(backspaceCompare("ab##", "c#d#"));
+//        System.out.println(remove("ab##"));
+//        System.out.println(remove("c#d#"));
+        System.out.println(remove("a##c"));
+        System.out.println(remove("#a#c"));
+//        StringBuilder stringBuilder = new StringBuilder("ab##");
+//        stringBuilder.delete(1, 3);
+//        System.out.println(stringBuilder);
+    }
+}
