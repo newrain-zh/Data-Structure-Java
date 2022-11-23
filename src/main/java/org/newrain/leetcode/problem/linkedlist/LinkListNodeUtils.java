13b1cb11799f076c64bed0a4719f31aa9510819d
newrain-zh <496268609@qq.com>
2022-11-23 23:30:30 +0800

    feat(all): 链表工具类 用于测试链表相关题目

---
+++ src/main/java/org/newrain/leetcode/problem/linkedlist/LinkListNodeUtils.java
@@ -0,0 +1,35 @@
+package org.newrain.leetcode.problem.linkedlist;
+
+/**
+ * 生成链表 打印链表相关工具类
+ *
+ * @author newrain
+ * @description 链表工具类
+ */
+public class LinkListNodeUtils {
+
+
+    public static ListNode get(int[] arr) {
+        ListNode curr = new ListNode(-1);
+        ListNode tail = new ListNode(arr[0]);
+        curr.next = tail;
+        for (int i = 1; i < arr.length; i++) {
+            ListNode listNode = new ListNode(arr[i]);
+            tail.next = listNode;
+            tail = listNode;
+        }
+        return curr.next;
+    }
+
+    public static void print(ListNode listNode) {
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        get(new int[]{1, 2, 3});
+    }
+
+}
