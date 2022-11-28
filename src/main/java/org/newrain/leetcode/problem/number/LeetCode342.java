919ee6315486fbca9bacadfffe816486d49256b6
newrain-zh <496268609@qq.com>
2022-11-28 21:06:16 +0800

    feat: 4的幂 isPowerOfFour

--- src/main/java/org/newrain/leetcode/problem/number/LeetCode342.java
+++ src/main/java/org/newrain/leetcode/problem/number/LeetCode342.java
@@ -2,12 +2,11 @@ package org.newrain.leetcode.problem.number;
 
 /**
  * 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
- * <p>
  * 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：<a href="https://leetcode.cn/problems/power-of-four">...</a>
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author NewRain
+ * @link <a href="https://leetcode.cn/problems/power-of-four">...</a>
+ * @description 4的幂
  */
 public class LeetCode342 {
 
