1a67992f2715041c19e3a6a13caea1adeb696732
zhiqin.zhang <zzqnobody@126.com>
2022-12-01 11:30:46 +0800

    feat: 加一 plusOne

--- src/main/java/org/newrain/code/problem/array/LeetCode66.java
+++ src/main/java/org/newrain/code/problem/array/LeetCode66.java
@@ -6,18 +6,22 @@ import java.util.Arrays;
  * @author NewRain
  * @description 加一
  * @complexity easy
- * @link <a href="https://leetcode.cn/problems/plus-one/">...</a>
+ * @link <a href="https://leetcode.cn/problems/plus-one/">leetcode</a>
+ * @link <a href="https://www.lintcode.com/problem/407">lintcode</a>
+ * @tag 数组 array
  */
 public class LeetCode66 {
 
     public static int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             digits[i]++;
+            //判断是否为加一10
             digits[i] = digits[i] % 10;
             if (digits[i] != 0) {
                 return digits;
             }
         }
+        //这里的情况为数组都为9的情况
         digits = new int[digits.length + 1];
         digits[0] = 1;
         return digits;
