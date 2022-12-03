a25a45c406c5cf5d54c52b7d31b33779b3963e61
newrain-zh <496268609@qq.com>
2022-12-03 23:11:35 +0800

    feat: 转小写 lowercaseToUppercase

---
+++ src/main/java/org/newrain/code/problem/base/LowercaseToUppercase.java
@@ -0,0 +1,14 @@
+package org.newrain.code.problem.base;
+
+public class LowercaseToUppercase {
+
+
+    public static char lowercaseToUppercase(char c) {
+        c ^= 32;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lowercaseToUppercase('a'));
+    }
+}
