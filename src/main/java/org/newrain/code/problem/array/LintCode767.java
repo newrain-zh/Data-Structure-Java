2fc058ef95e8b7547f8f320b585b9de6b945de33
zhiqin.zhang <zzqnobody@126.com>
2022-12-03 23:07:23 +0800

    feat: 翻转数组 reverseArray

---
+++ src/main/java/org/newrain/code/problem/array/LintCode767.java
@@ -0,0 +1,28 @@
+package org.newrain.code.problem.array;
+
+import java.util.Arrays;
+
+/**
+ * @author NewRain
+ * @description 翻转数组
+ * @link <a href="https://www.lintcode.com/problem/767">lintcode</a>
+ * @tag 数组 Array
+ */
+public class LintCode767 {
+
+    public static void reverseArray(int[] nums) {
+        // write your code here
+        int middle = (nums.length / 2);
+        for (int i = 0; i < middle; i++) {
+            int tempValue = nums[i];
+            nums[i] = nums[(nums.length - 1) - i];
+            nums[(nums.length - 1) - i] = tempValue;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        reverseArray(new int[]{1, 2, 3, 4});
+        reverseArray(new int[]{1, 2, 5});
+    }
+}
