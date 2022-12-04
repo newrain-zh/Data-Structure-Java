caabcaccf9262789e25afce0d995cfb54b8be2c8
newrain-zh <496268609@qq.com>
2022-12-04 22:27:19 +0800

    feat: 小括号匹配 matchParentheses

---
+++ src/main/java/org/newrain/code/problem/string/LintCode263.java
@@ -0,0 +1,36 @@
+package org.newrain.code.problem.string;
+
+import java.util.Stack;
+
+/**
+ * @author NewRain
+ * @description 小括号匹配
+ * @link <a href="https://www.lintcode.com/problem/263">lintcode</a>
+ */
+public class LintCode263 {
+
+    /**
+     * 利用栈来解决匹配问题
+     *
+     * @param string
+     * @return boolean
+     */
+    public boolean matchParentheses(String string) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = string.toCharArray();
+        for (char aChar : chars) {
+            if (')' == aChar) {
+                //说明出现不对称情况
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+
+            } else if ('(' == aChar) {
+                stack.push('(');
+            }
+        }
+        return stack.isEmpty();
+    }
+}
