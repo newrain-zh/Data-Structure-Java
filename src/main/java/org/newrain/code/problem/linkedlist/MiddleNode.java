62eed5a1c6754d179c5d99bb4ce4a325bb2629b7
newrain-zh <496268609@qq.com>
2022-12-20 14:05:50 +0800

    链表的中间结点 MiddleNode

---
+++ src/main/java/org/newrain/code/problem/linkedlist/MiddleNode.java
@@ -0,0 +1,54 @@
+package org.newrain.code.problem.linkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author NewRain
+ * @description 链表的中间结点
+ * @link <a href="https://www.lintcode.com/problem/1609">lintcode</a>
+ */
+public class MiddleNode {
+
+    /**
+     * 使用数组顺序存储节点
+     *
+     * @param head
+     * @return
+     */
+    public ListNode middleNode(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        ListNode tail = head;
+        while (tail.next != null) {
+            list.add(tail);
+            tail = tail.next;
+        }
+        int pos = list.size() / 2;
+        if ((list.size() & 1) != 0) {
+            return list.get(pos);
+        }
+        return list.get(pos + 1);
+    }
+
+    /**
+     * 快慢指针解法
+     *
+     * @param head
+     * @return
+     */
+    public ListNode middleNode1(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(7 / 2);
+    }
+
+
+}
