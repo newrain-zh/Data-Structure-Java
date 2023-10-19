426a96116a5472b6e6f2379c9f9c376c09c848f3
newrain-zh <496268609@qq.com>
2023-10-19 18:08:51 +0800

    数组的最大值

---
+++ src/main/java/org/newrain/code/problem/array/MaxOfArray.java
@@ -0,0 +1,17 @@
+package org.newrain.code.problem.array;
+
+public class MaxOfArray {
+
+    /**
+     * @param a: an array of float numbers
+     * @return: a float number
+     */
+    public float maxOfArray(float[] a) {
+        //设置为负无穷大
+        float result = Float.NEGATIVE_INFINITY;
+        for (float v : a) {
+            result = Math.max(result, v);
+        }
+        return result;
+    }
+}
