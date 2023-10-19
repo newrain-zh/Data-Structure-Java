b1b3c1b5c80b226711eaa6651ca92d0f5674d722
newrain-zh <496268609@qq.com>
2023-10-19 17:48:51 +0800

    寻找最大值

---
+++ src/main/java/org/newrain/code/problem/array/MaxNum.java
@@ -0,0 +1,28 @@
+package org.newrain.code.problem.array;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * lint code 297题
+ */
+public class MaxNum {
+
+
+    /**
+     * @param nums: the list of numbers
+     * @return: return the maximum number.
+     */
+    public int maxNum(List<Integer> nums) {
+        int maxNum = Integer.MIN_VALUE;
+        for (Integer num : nums) {
+            maxNum = Math.max(maxNum, num);
+        }
+        return maxNum;
+    }
+
+    public int maxNum1(List<Integer> nums) {
+        Comparator<Integer> comparator = Comparator.comparing(Integer::intValue);
+        return nums.stream().max(comparator).orElse(0);
+    }
+}
