53214638f4e132ab8bf7ce5c7c2f3f3e0e78ebec
alex <zzqnobody@126.com>
2023-11-09 16:59:04 +0800

    检测大写字母

--- src/main/java/org/newrain/code/problem/string/DetectCapitalUse.java
+++ src/main/java/org/newrain/code/problem/string/DetectCapitalUse.java
@@ -12,8 +12,9 @@ package org.newrain.code.problem.string;
  * 来源：力扣（LeetCode）
  * 链接：<a href="https://leetcode.cn/problems/detect-capital">...</a>
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @description 检测大写字母
  */
-public class LeetCode520 {
+public class DetectCapitalUse {
 
 
     public static boolean detectCapitalUse(String word) {
