1e81d9676db7ef1c531d8570f37d4274bd67fcdf
alex <zzqnobody@126.com>
2023-11-21 11:40:43 +0800

    有序数组中出现次数超过25%的元素

--- src/main/java/org/newrain/code/problem/array/FindSpecialInteger.java
+++ src/main/java/org/newrain/code/problem/array/FindSpecialInteger.java
@@ -5,11 +5,11 @@ package org.newrain.code.problem.array;
  * 给你一个非递减的 有序 整数数组，已知这个数组中恰好有一个整数，它的出现次数超过数组元素总数的 25%。
  * 请你找到并返回这个整数
  *
- * @author NewRain
+ * @author alex
  * @description 有序数组中出现次数超过25%的元素
  * @link <a href="https://leetcode.cn/problems/element-appearing-more-than-25-in-sorted-array/"></a>
  */
-public class LeetCode1287 {
+public class FindSpecialInteger {
 
 
     public static int findSpecialInteger(int[] arr) {
