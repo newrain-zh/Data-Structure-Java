63958fbec5e5355eef8a537d7e1bb21d19a10f14
alex <zzqnobody@126.com>
2023-11-23 18:41:18 +0800

    leetcod.350 两个数组的交集 II

--- src/main/java/org/newrain/code/problem/array/IntersectII.java
+++ src/main/java/org/newrain/code/problem/array/IntersectII.java
@@ -1,8 +1,11 @@
 package org.newrain.code.problem.array;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
+ * leetcod.350 两个数组的交集 II
  * 给你两个整数数组 nums1 和 nums2 ，
  * 请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，
  * 应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。
@@ -10,7 +13,7 @@ import java.util.*;
  *
  * @author newrain
  */
-public class LeetCode350 {
+public class IntersectII {
 
     /**
      * 解法1 利用集合
