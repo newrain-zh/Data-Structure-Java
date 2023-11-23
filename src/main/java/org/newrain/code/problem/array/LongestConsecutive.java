796a8fe57b7f8e112ea73f99789c4d1e72e4767f
alex <zzqnobody@126.com>
2023-11-23 17:14:30 +0800

    最长数字连续序列

--- src/main/java/org/newrain/code/problem/array/LongestConsecutive.java
+++ src/main/java/org/newrain/code/problem/array/LongestConsecutive.java
@@ -20,7 +20,7 @@ import java.util.Set;
  * <p>
  * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
  */
-public class LeetCode128 {
+public class LongestConsecutive {
 
 
     public static int longestConsecutive1(int[] nums) {
