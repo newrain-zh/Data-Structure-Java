8eb5e7737431cb8dceb76e82e1997f0aa74895ff
alex <zzqnobody@126.com>
2023-11-23 18:38:19 +0800

    leetcode.217 存在重复元素

--- src/main/java/org/newrain/code/problem/array/ContainsDuplicate.java
+++ src/main/java/org/newrain/code/problem/array/ContainsDuplicate.java
@@ -4,12 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ *
+ * leetcode.217 存在重复元素
  * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；
  * 如果数组中每个元素互不相同，返回 false 。
  *
  * @author newrain
  */
-public class LeetCode217 {
+public class ContainsDuplicate {
 
 
     public boolean containsDuplicate(int[] nums) {
