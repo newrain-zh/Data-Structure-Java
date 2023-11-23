d24eee8a56f1e3c60346d18f696934ee185feeb4
newrain-zh <496268609@qq.com>
2023-11-23 15:49:21 +0800

    基于排列构建数组

---
+++ src/main/java/org/newrain/code/problem/array/BuildArray.java
@@ -0,0 +1,22 @@
+package org.newrain.code.problem.array;
+
+import java.util.Arrays;
+
+/**
+ * 基于排列构建数组
+ */
+public class BuildArray {
+
+
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(buildArray(new int[]{0, 2, 1, 5, 3, 4})));
+    }
+}
