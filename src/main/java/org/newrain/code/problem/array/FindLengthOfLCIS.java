8e69bdb32524907a200c6b310465d5799dd1eaaa
newrain-zh <496268609@qq.com>
2023-11-23 17:18:55 +0800

    最长连续递增序列

--- src/main/java/org/newrain/code/problem/array/FindLengthOfLCIS.java
+++ src/main/java/org/newrain/code/problem/array/FindLengthOfLCIS.java
@@ -1,6 +1,8 @@
 package org.newrain.code.problem.array;
 
 /**
+ *
+ * 题目：最长连续递增序列
  * 给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
  * <p>
  * 连续递增的子序列 可以由两个下标 l 和 r（l < r）确定，如果对于每个 l <= i < r，都有 nums[i] < nums[i + 1] ，那么子序列 [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] 就是连续递增子序列。
@@ -9,7 +11,7 @@ package org.newrain.code.problem.array;
  * 链接：https://leetcode.cn/problems/longest-continuous-increasing-subsequence
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class LeetCode674 {
+public class FindLengthOfLCIS {
 
 
     public static int findLengthOfLCIS(int[] nums) {
