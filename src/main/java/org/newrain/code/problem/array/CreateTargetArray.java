772e340c152c1837460350c6cefc2ebb52b822f7
alex <zzqnobody@126.com>
2023-11-23 13:35:11 +0800

    按既定顺序创建目标数组

---
+++ src/main/java/org/newrain/code/problem/array/CreateTargetArray.java
@@ -0,0 +1,31 @@
+package org.newrain.code.problem.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 题目：按既定顺序创建目标数组
+ *
+ *
+ */
+public class CreateTargetArray {
+
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        int[] ans = new int[nums.length];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        createTargetArray(new int[]{0, 1, 2, 3, 4}, new int[]{0,1,2,2,1});
+    }
+
+}
