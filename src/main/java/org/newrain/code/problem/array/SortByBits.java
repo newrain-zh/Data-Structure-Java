81a5f41469d7d340bd28150fc654be008b7ec650
newrain-zh <496268609@qq.com>
2023-12-04 12:02:40 +0800

    leetcode.1356 根据数字二进制下 1 的数目排序

--- src/main/java/org/newrain/code/problem/array/SortByBits.java
+++ src/main/java/org/newrain/code/problem/array/SortByBits.java
@@ -55,6 +55,23 @@ public class SortByBits {
         return cnt;
     }
 
+    /**
+     * TODO 待研究的最优解方案
+     * @param arr
+     * @return
+     */
+    public int[] sortByBits1(int[] arr) {
+        int[] map = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            map[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(map);
+        for (int i = 0; i < map.length; i++) {
+            map[i] = map[i] % 10000000;
+        }
+        return map;
+    }
+
     public static void main(String[] args) {
         int a = 10 ^ 4;
         System.out.println(a);
