40eab9d16c0a2dba7dc435e84922426042446f95
newrain-zh <496268609@qq.com>
2023-12-11 13:16:24 +0800

    leetcode.387 字符串中的第一个唯一字符

--- src/main/java/org/newrain/code/problem/string/LeetCode387.java
+++ src/main/java/org/newrain/code/problem/string/LeetCode387.java
@@ -4,19 +4,17 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
+ * leetcode.387 字符串中的第一个唯一字符
  * 给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
+ *
  * @author newrain
  */
 public class LeetCode387 {
 
 
     public static int firstUniqChar(String s) {
-        if (s == null || s.length() == 0) {
-            return -1;
-        }
-        if (s.length() == 1) {
-            return 0;
-        }
+        if (s == null || s.isEmpty()) return -1;
+        if (s.length() == 1) return 0;
         char[] chars = s.toCharArray();
         Map<Character, Integer> map = new LinkedHashMap<>(chars.length);
         for (char aChar : chars) {
@@ -37,5 +35,7 @@ public class LeetCode387 {
 
     public static void main(String[] args) {
         System.out.println(firstUniqChar("loveleetcode"));
+
+        System.out.println("source".indexOf("rced".charAt(0)));
     }
 }
