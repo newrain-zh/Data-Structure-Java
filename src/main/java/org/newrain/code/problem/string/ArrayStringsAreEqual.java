04c72081a982cd6c81c7db892f2fa70b4b906eda
newrain-zh <496268609@qq.com>
2023-12-11 15:07:31 +0800

    leetCode.1662 检查两个字符串数组是否相等

--- src/main/java/org/newrain/code/problem/string/ArrayStringsAreEqual.java
+++ src/main/java/org/newrain/code/problem/string/ArrayStringsAreEqual.java
@@ -1,12 +1,13 @@
 package org.newrain.code.problem.string;
 
 /**
+ * leetCode.1662 检查两个字符串数组是否相等
  * @author NewRain
  * @description 检查两个字符串数组是否相等
  * @link <a href="https://leetcode.cn/problems/check-if-two-string-arrays-are-equivalent/"></a>
  * @tag 字符串 String
  */
-public class LeetCode1662 {
+public class ArrayStringsAreEqual {
 
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         if (word1[0].charAt(0) != word2[0].charAt(0)) {
