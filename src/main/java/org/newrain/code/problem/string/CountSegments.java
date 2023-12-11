c28cfa63bbb11317d58a3b19430c395d88d4e0f8
newrain-zh <496268609@qq.com>
2023-12-11 13:20:22 +0800

    leetcode.434 字符串中的单词数

--- src/main/java/org/newrain/code/problem/string/CountSegments.java
+++ src/main/java/org/newrain/code/problem/string/CountSegments.java
@@ -1,20 +1,23 @@
 package org.newrain.code.problem.string;
 
-public class LeetCode434 {
+/**
+ * leetcode.434 字符串中的单词数
+ */
+public class CountSegments {
 
     public static int countSegments(String s) {
-        if (s == null || s.length() == 0) {
+        if (s == null || s.isEmpty()) {
             return 0;
         }
         String[] s1 = s.split(" ");
         int count = 0;
         for (String str : s1) {
-            if (str.length() == 0) {
+            if (str.isEmpty()) {
                 continue;
             }
             if (str.contains(" ")) {
                 String replace = str.replace(" ", "");
-                if (replace.length() > 0) {
+                if (!replace.isEmpty()) {
                     count = count + 1;
                 }
             } else {
