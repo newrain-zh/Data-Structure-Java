a32b40dbd8472b02a0352c4e9dba49a8bffd7494
newrain-zh <496268609@qq.com>
2023-12-18 22:11:31 +0800

    leetcode.977 有序的平方数组

---
+++ src/main/java/org/newrain/code/problem/array/SortedSquares.java
@@ -0,0 +1,24 @@
+package org.newrain.code.problem.array;
+
+import java.util.Arrays;
+
+/**
+ * leetcode.977 有序的平方数组
+ * 给你一个按 非递减顺序 排序的整数数组 nums，
+ * 返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+ */
+public class SortedSquares {
+
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((-4)^2);
+    }
+
+}
