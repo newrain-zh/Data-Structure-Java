f579c355df136e044a638b8bc541f13c3062a1f8
newrain-zh <496268609@qq.com>
2023-12-22 18:03:29 +0800

    leetcode.559. N 叉树的最大深度

---
+++ src/main/java/org/newrain/code/problem/tree/NtreeMaxDepth.java
@@ -0,0 +1,24 @@
+package org.newrain.code.problem.tree;
+
+import java.util.List;
+
+/**
+ * leetcode.559  N 叉树的最大深度
+ */
+public class NtreeMaxDepth {
+
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxChildDepth = 0;
+        List<Node> children = root.children;
+        for (Node child : children) {
+            int childDepth = maxDepth(child);
+            maxChildDepth = Math.max(maxChildDepth, childDepth);
+        }
+        return maxChildDepth + 1;
+
+    }
+}
