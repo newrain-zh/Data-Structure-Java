554bdf9cad7f73cce58dc13a02234883b13b85db
alex <zzqnobody@126.com>
2023-12-22 18:30:37 +0800

    leetcode.671 二叉树中第二小的节点

---
+++ src/main/java/org/newrain/code/problem/tree/FindSecondMinimumValue.java
@@ -0,0 +1,45 @@
+package org.newrain.code.problem.tree;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * leetcode.671 二叉树中第二小的节点
+ * @link <a href="https://leetcode.cn/problems/second-minimum-node-in-a-binary-tree/description/">...</a>
+ */
+public class FindSecondMinimumValue {
+
+    public int findSecondMinimumValue(TreeNode root) {
+        Set<Integer> list = new HashSet<>();
+        foreach(root, list);
+        if (list.size() <= 1) {
+            return -1;
+        }
+        List<Integer> list1 = new ArrayList<>(list);
+        int max = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+        for (Integer i : list1) {
+            if (i <= max) {
+                second = max;
+                max = i;
+            } else {
+                if (i <= second) {
+                    second = i;
+                }
+            }
+        }
+        return second;
+
+    }
+
+
+    public void foreach(TreeNode node, Set<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        list.add(node.val);
+        foreach(node.left, list);
+        foreach(node.right, list);
+    }
+}
