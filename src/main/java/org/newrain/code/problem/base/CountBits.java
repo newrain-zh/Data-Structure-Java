fad3e8acf817fc076d11ca29e16c61e26cc4fd53
alex <zzqnobody@126.com>
2023-12-23 16:53:56 +0800

    leetcode. LCR 003. 比特位计数

---
+++ src/main/java/org/newrain/code/problem/base/CountBits.java
@@ -0,0 +1,41 @@
+package org.newrain.code.problem.base;
+
+import java.util.Arrays;
+
+
+/**
+ * leetcode
+ * LCR 003. 比特位计数
+ *
+ *
+ */
+public class CountBits {
+    public static int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            ans[i] = bitmapCount(i);
+        }
+        return ans;
+    }
+
+    /**
+     * 统计一个二进制为1的个数
+     * @param x
+     * @return
+     */
+    public static int bitmapCount(int x) {
+        int cnt = 0;
+        while (x > 0) {
+            x = x & (x - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(2)));
+    }
+
+
+}
