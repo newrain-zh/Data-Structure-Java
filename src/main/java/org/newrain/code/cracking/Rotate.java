d5741c302479f8a837050474217fb32c3fcff0b0
newrain-zh <496268609@qq.com>
2024-12-04 21:46:22 +0800

    旋转矩阵

--- src/main/java/org/newrain/code/cracking/Rotate.java
+++ src/main/java/org/newrain/code/cracking/Rotate.java
@@ -51,8 +51,7 @@ public class Rotate {
     public static void main(String[] args) {
         int[][] temp = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         rotate(temp);
-        for (int i = 0; i < temp.length; i++) {
-            int[] ints = temp[i];
+        for (int[] ints : temp) {
             for (int n : ints) {
                 System.out.print(n + ",");
             }
