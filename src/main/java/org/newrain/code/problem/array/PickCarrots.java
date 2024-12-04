e3731109ff6004555d75e1e8707cc028e4cd3fc3
newrain-zh <496268609@qq.com>
2024-12-04 21:41:33 +0800

    PickCarrots

---
+++ src/main/java/org/newrain/code/problem/array/PickCarrots.java
@@ -0,0 +1,14 @@
+package org.newrain.code.problem.array;
+
+public class PickCarrots {
+
+
+    /**
+     * @param carrot: an integer matrix
+     * @return Return the number of steps that can be moved.
+     */
+    public int pickCarrots(int[][] carrot) {
+        //先计算中心
+        return 0;
+    }
+}
