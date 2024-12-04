87351c52d2440eb6c45f7ee926bafd6912cb55a6
newrain-zh <496268609@qq.com>
2024-12-04 21:49:32 +0800

    IsInterleave

---
+++ src/main/java/org/newrain/code/problem/dp/IsInterleave.java
@@ -0,0 +1,15 @@
+package org.newrain.code.problem.dp;
+
+public class IsInterleave {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length(), n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+
+        return false;
+
+
+    }
+}
