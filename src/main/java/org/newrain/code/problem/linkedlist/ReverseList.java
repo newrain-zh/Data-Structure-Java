ee57a55aaac89152bee47c514df26128bf5ad655
newrain-zh <496268609@qq.com>
2025-03-06 13:55:24 +0800

    反转链表

--- src/main/java/org/newrain/code/problem/linkedlist/ReverseList.java
+++ src/main/java/org/newrain/code/problem/linkedlist/ReverseList.java
@@ -15,12 +15,9 @@ public class ReverseList {
         //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
         while (curr != null) {
             //2
-            ListNode next = curr.next;
-            //下一节点的指针指向上一节点
-            curr.next = prev;
-            //上一节点为当前节点
-            prev = curr;
-            //获取下一节点
+            ListNode next = curr.next; // 下一节点的指针指向上一节点
+            curr.next = prev;          // 上一节点为当前节点
+            prev = curr;        // 获取下一节点
             curr = next;
         }
         return prev;
