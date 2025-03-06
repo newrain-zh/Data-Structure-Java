4a8fbc457149a84c93d7c7cd2fcb0016b2b6f9cf
newrain-zh <496268609@qq.com>
2025-03-06 13:48:35 +0800

    LeetCode46

---
+++ src/main/java/org/newrain/code/problem/backtracking/LeetCode46.java
@@ -0,0 +1,41 @@
+package org.newrain.code.problem.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author NewRain
+ * <a href="https://leetcode.cn/problems/permutations/description/">...</a>
+ */
+public class LeetCode46 {
+
+
+    /**
+     * nums 规则
+     * 1 <= nums.length <= 6
+     * -10 <= nums[i] <= 10
+     * nums 中的所有整数 互不相同
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 1) {
+            List<Integer> list = new ArrayList<>();
+            list.add(nums[0]);
+            res.add(list);
+            return res;
+        }
+        return null;
+    }
+
+    public List<Integer> getPermute(int[] nums, int index) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+        }
+        return list;
+    }
+
+
+}
