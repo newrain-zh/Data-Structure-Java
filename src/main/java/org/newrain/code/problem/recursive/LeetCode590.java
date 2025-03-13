1b73c113b3a4145f1bf61fedaa7b3b404c1df9d1
newrain-zh <496268609@qq.com>
2025-03-13 23:29:04 +0800

    LeetCode 590

--- src/main/java/org/newrain/code/problem/recursive/LeetCode590.java
+++ src/main/java/org/newrain/code/problem/recursive/LeetCode590.java
@@ -1,6 +1,6 @@
 package org.newrain.code.problem.recursive;
 
-import org.newrain.code.problem.common.Node;
+import org.newrain.code.problem.common.NTreeNode;
 import org.newrain.code.problem.utils.NTreeUtils;
 
 import java.util.ArrayList;
@@ -12,27 +12,27 @@ import java.util.List;
  */
 public class LeetCode590 {
 
-    public static List<Integer> postorder(Node root) {
+    public static List<Integer> postorder(NTreeNode root) {
         List<Integer> res = new ArrayList<>();
         dfs(root, res);
         return res;
     }
 
-    public static void dfs(Node root, List<Integer> list) {
+    public static void dfs(NTreeNode root, List<Integer> list) {
         if (root == null) {
             return;
         }
         System.out.println(root.val);
         //后序遍历的顺序为左-> 右 -> 根
-        List<Node> children = root.children;
-        for (Node child : children) {
+        List<NTreeNode> children = root.children;
+        for (NTreeNode child : children) {
             dfs(child, list);
         }
         list.add(root.val);
     }
 
     public static void main(String[] args) {
-        Node node = NTreeUtils.buildTree(Arrays.asList(1, null, 3, 2, 4, null, 5, 6));
-        System.out.println(postorder(node));
+        NTreeNode NTreeNode = NTreeUtils.buildTree(Arrays.asList(1, null, 3, 2, 4, null, 5, 6));
+        System.out.println(postorder(NTreeNode));
     }
 }
