93a5b00859928cdc6af09f4b2422bfb60b4a2671
newrain-zh <496268609@qq.com>
2025-03-20 10:29:55 +0800

    LeetCode.303 区域和检索 - 数组不可变

--- src/main/java/org/newrain/problems/problem/array/LeetCode303.java
+++ src/main/java/org/newrain/problems/problem/array/LeetCode303.java
@@ -1,5 +1,7 @@
 package org.newrain.problems.problem.array;
 
+import java.util.Arrays;
+
 /**
  * 303. 区域和检索 - 数组不可变
  */
@@ -12,17 +14,23 @@ public class LeetCode303 {
         for (int i = 0; i < nums.length; i++) {
             sums[i + 1] = sums[i] + nums[i]; // 提前计算好每一位的前缀和
         }
+        System.out.println(Arrays.toString(sums));
     }
 
     /**
      * 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，
      * 包含 left 和 right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
-     *
+     *     -2,  0, 3, -5, 2, -1
+     * [0, -2, -2, 1, -4, -2, -3]
      * @param left
      * @param right
      * @return
      */
     public int sumRange(int left, int right) {
         return sums[right + 1] - sums[left];
+    }
+
+    public static void main(String[] args) {
+        LeetCode303 leetCode303 = new LeetCode303(new int[]{-2, 0, 3, -5, 2, -1});
     }
 }
