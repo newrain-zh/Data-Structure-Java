e25c337d2a92d9a724edf3c5bc40ab7bc53627bb
newrain-zh <496268609@qq.com>
2025-04-19 00:39:29 +0800

    组合

--- src/main/java/org/newrain/problems/problem/backtracking/LeetCode77.java
+++ src/main/java/org/newrain/problems/problem/backtracking/LeetCode77.java
@@ -1,6 +1,7 @@
 package org.newrain.problems.problem.backtracking;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +9,25 @@ import java.util.List;
  * <a href="https://leetcode.cn/problems/combinations/description">...</a>
  */
 public class LeetCode77 {
-
+    List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
-
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
         return null;
+    }
+
+    public void backtracking(int[] arr, boolean[] used, List<Integer> path) {
+        for (int i = 0; i < arr.length; i++) {
+            if (!used[i]){
+                used[i] = true;
+                path.add(arr[i]);
+                backtracking(arr, used, path);
+                used[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
     }
 }
