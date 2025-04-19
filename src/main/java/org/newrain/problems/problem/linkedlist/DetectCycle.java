c37ba3d85759f82a2a4f47870dc5d5075427b6f2
newrain-zh <496268609@qq.com>
2025-04-19 22:13:34 +0800

    环形链表 II

--- src/main/java/org/newrain/problems/problem/linkedlist/DetectCycle.java
+++ src/main/java/org/newrain/problems/problem/linkedlist/DetectCycle.java
@@ -1,9 +1,10 @@
 package org.newrain.problems.problem.linkedlist;
 
 import org.newrain.problems.problem.common.ListNode;
+import org.newrain.problems.problem.utils.LinkListNodeUtils;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author NewRain
@@ -13,16 +14,43 @@ import java.util.Set;
  */
 public class DetectCycle {
 
-    public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while (head != null) {
-            if (!set.contains(head)) {
-                set.add(head);
-            } else {
-                return head;
+    /**
+     * 快慢指针
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode pt1 = head;
+                while (pt1 != slow) {
+                    pt1  = pt1.next;
+                    slow = slow.next;
+                }
+                return slow;
             }
-            head = head.next;
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode       treeNode = LinkListNodeUtils.get(new int[]{1, 2, 3, 4, 5});
+        List<ListNode> list     = new ArrayList<>();
+        ListNode       curr     = treeNode;
+        while (curr != null) {
+            list.add(curr);
+            curr = curr.next;
+        }
+        list.get(list.size() - 1).next = list.get(1);
+        ListNode listNode = detectCycle(treeNode);
+        System.out.println(listNode.val);
     }
 }
