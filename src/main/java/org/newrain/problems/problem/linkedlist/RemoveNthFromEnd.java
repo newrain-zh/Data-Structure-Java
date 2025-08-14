5b0d42df849841baf95c03ceb9b1dcf27a04ad06
newrain-zh <496268609@qq.com>
2025-08-14 08:09:06 +0800

    RemoveNthFromEnd

--- src/main/java/org/newrain/problems/problem/linkedlist/RemoveNthFromEnd.java
+++ src/main/java/org/newrain/problems/problem/linkedlist/RemoveNthFromEnd.java
@@ -1,6 +1,7 @@
 package org.newrain.problems.problem.linkedlist;
 
 import org.newrain.problems.problem.common.ListNode;
+import org.newrain.problems.problem.utils.LinkListNodeUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,7 @@ public class RemoveNthFromEnd {
         if (head == null) {
             return head;
         }
-        ListNode curr = head;
+        ListNode       curr = head;
         List<ListNode> list = new ArrayList<>();
         while (curr != null) {
             list.add(curr);
@@ -27,7 +28,7 @@ public class RemoveNthFromEnd {
         if (list.size() == 1) {
             return null;
         }
-        int pos = list.size() - n;
+        int      pos        = list.size() - n;
         ListNode deleteNode = list.get(pos);
         //[1,2] 1
         if (pos - 1 >= 0) {
@@ -37,6 +38,35 @@ public class RemoveNthFromEnd {
         } else {
             return list.get(1);
         }
+    }
+
+    public static ListNode removeNthFromEnd1(ListNode head, int n) {
+        if (head == null || head.next == null) return null;
+        ListNode dummy = head;
+        int      len   = 0;
+        while (dummy != null) {
+            len++;
+            dummy = dummy.next;
+        }
+        int pos = len - n;
+        if (pos == 0) { // 如果为 0 则说明要删除头节点
+            return head.next;
+        }
+        dummy = head;
+        int      count = 0;
+        ListNode pre;
+        while (dummy != null) {
+            count++;
+            pre   = dummy;
+            dummy = dummy.next;
+            if (count == pos) pre.next = dummy.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+//        LinkListNodeUtils.print(removeNthFromEnd1(LinkListNodeUtils.get(new int[]{1, 2, 3, 4, 5}), 2));
+        LinkListNodeUtils.print(removeNthFromEnd1(LinkListNodeUtils.get(new int[]{1, 2}), 2));
     }
 
 }
